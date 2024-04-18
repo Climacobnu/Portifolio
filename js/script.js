d9375be2a193d4a50fd0a74c11d09106313c7c7a
Leandro Climaco <climacobnu@gmail.com>
2024-04-18 14:01:24 -0300

    .

--- js/script.js
+++ js/script.js
@@ -57,7 +57,7 @@ ScrollReveal().reveal('.home-content p, .about-content',  { origin: 'right' });
   /* typed js*/
   
   const typed = new Typed('.multiple-text', {
-    strings: ['Desenvolvedor Front-end', 'e Desenvolvedor Back-end'],
+    strings: ['Desenvolvedor Full-Stack!'],
     typeSpeed: 100,
     backSpeed: 100,
     backDelay: 100,
@@ -89,5 +89,14 @@ ScrollReveal().reveal('.home-content p, .about-content',  { origin: 'right' });
 
   // ========================================
 
-
+  var btn = document.getElementById('btn-div');
+  var container = document.querySelector('.container');
+  btn.addEventListener('click', function() {
+      
+    if(container.style.display === 'block') {
+        container.style.display = 'none';
+    } else {
+        container.style.display = 'block';
+    }
+  });
   
